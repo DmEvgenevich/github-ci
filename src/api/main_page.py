a49fe0228368bbe070bd62ea07bc314f27135787
Galchenko Dmitriy <galchenkode725@gmail.com>
2025-08-16 18:49:25 +0300

    Создание моделей

---
+++ src/api/main_page.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+from core.schemas.main_page import MainBase
+
+main_route = APIRouter()
+
+
+@main_route.get("/", tags=["Главная страница"], response_model=MainBase)
+def main_page():
+    return {"message": "Main page"}
--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from contextlib import asynccontextmanager
 
 from core.config import settings
 from core.models import db_helper
+from api.main_page import main_route
+from api.recipes import recipe_route
 
 
 @asynccontextmanager
@@ -12,12 +14,13 @@ async def lifespan(app: FastAPI):
     await db_helper.dispose()
 
 main_app = FastAPI(lifespan=lifespan)
-main_app.include_router(api_router)
+main_app.include_router(main_route)
+main_app.include_router(recipe_route)
 
 if __name__ == "__main__":
     uvicorn.run(
         "main:main_app",
         host=settings.run.host,
         port=settings.run.port,
-        reload=True
+        reload=True,
     )
